88d748be21a80ba1db77916bcbf61165f471b27a
Festongithub <Fkemboik@gmail.com>
2024-01-17 12:54:03 +0300

    initial commit

---
+++ restaurant/target/debug/deps/restaurant-66ed86241561f9c6.d
@@ -0,0 +1,7 @@
+/home/macron/100_days-of-Rust/restaurant/target/debug/deps/restaurant-66ed86241561f9c6.rmeta: src/lib.rs
+
+/home/macron/100_days-of-Rust/restaurant/target/debug/deps/librestaurant-66ed86241561f9c6.rlib: src/lib.rs
+
+/home/macron/100_days-of-Rust/restaurant/target/debug/deps/restaurant-66ed86241561f9c6.d: src/lib.rs
+
+src/lib.rs:
---
+++ restaurant/target/debug/librestaurant.d
@@ -0,0 +1,1 @@
+/home/macron/100_days-of-Rust/restaurant/target/debug/librestaurant.rlib: /home/macron/100_days-of-Rust/restaurant/src/lib.rs
